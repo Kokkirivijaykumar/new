b3157ea0af2979256468223b8928f075002c29c0
vijay kumar <kokkirivijaykumar@gmail.com>
2021-12-05 23:43:02 +0530

    new

---
+++ New1.java
@@ -0,0 +1,29 @@
+package New;
+class New1 { 
+public static void main(String[] args) {
+		public class Account{
+
+			private int accountNumber;
+			private String customerName;
+			private double balance;
+			private String customerEmailAddress;
+			private String customerPhoneNumber;
+			private String debitCardNumber;
+
+		public Account(int accountNumber, String customerName, double balance, String customerEmailAddress,
+					String customerPhoneNumber, String debitCardNumber) {
+
+				this.accountNumber = accountNumber;
+				this.customerName = customerName;
+				this.balance = balance;
+				this.customerEmailAddress = customerEmailAddress;
+				this.customerPhoneNumber = customerPhoneNumber;
+				this.debitCardNumber = debitCardNumber;
+
+				System.out.println("main constructor called");
+			}
+		}
+ }
+
+		
+
---
+++ New2.java
@@ -0,0 +1,9 @@
+package New;
+
+public class New2 {
+	class MainMethod {
+
+		public static void main(String[]args) {
+	Account anshu = new Account(101, "Jeigyanshu", 1008981828, "Jeigyanshu@gmail.com", "7019344265");
+		}
+}
